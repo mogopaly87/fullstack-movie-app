d6da7d0027b6bb45e3929691154336ca6d7e8373
Chinonso Mogo <mogononso@gmail.com>
2023-03-17 21:06:57 -0230

    added setup bash file

---
+++ setup.sh
@@ -0,0 +1,8 @@
+cd ~/fullstack-movie-app/movie-ratings-app
+npm install
+npm run build
+mv build/ ~/fullstack-movie-app/my-ratings-backend
+
+cd ~/fullstack-movie-app/my-ratings-backend
+npm install
+node src/server.js
