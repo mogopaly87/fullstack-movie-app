aa633bdd1d6976a78612855071e3d20674fbfa47
Chinonso Mogo <mogononso@gmail.com>
2023-03-17 15:16:09 -0230

    completed setup for production

--- my-ratings-backend/src/db.js
+++ my-ratings-backend/src/db.js
@@ -1,10 +1,13 @@
 
 import {MongoClient} from 'mongodb';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 let db;
 
 async function connectToDb(cb) {
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    const client = new MongoClient(process.env.MONGO_CONNECT);
+    // const client = new MongoClient('mongodb://127.0.0.1:27017');
     await client.connect();
 
     db = client.db('ratingsdb')
@@ -15,3 +18,4 @@ export {
     db,
     connectToDb,
 }
+
